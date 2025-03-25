80f1e1af1367497bf6116d77b4268afa5e762d9f
pecud <pecudibnuliany@gmail.com>
2025-03-25 22:33:40 +0700

    Install package Select2 and Create CRUD View User

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::middleware('auth')->group(function () {
     // roles route
     Route::resource('roles', \App\Http\Controllers\RoleController::class)->except('show');
 
+    // users route
+    Route::resource('/users', \App\Http\Controllers\UserController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
